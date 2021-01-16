a149d4b022f0de7480c8bbd3deeaab82c7455b37
Rui Zhu <zhuruisy@gmail.com>
2021-01-16 13:50:09 -0500

    changes

---
+++ Assets/zombie_Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_Move : MonoBehaviour
+{
+    private float _userHorizInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
