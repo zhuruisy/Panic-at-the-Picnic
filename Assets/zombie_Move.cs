f9e6753f30bcef383144a23146aeb693b54aadf0
Rui Zhu <zhuruisy@gmail.com>
2021-01-16 14:29:56 -0500

    zombie movie script

--- Assets/zombie_Move.cs
+++ Assets/zombie_Move.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class zombie_Move : MonoBehaviour
 {
-    private float _userHorizInput;
+    private float _vertInput;
+    private float _rotationInput;
+    private Vector3 _userRot;
+
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _vertInput = Input.GetAxis("Vertical");
+        _rotationInput = Input.GetAxis("Horizontal");
+        Debug.Log(_rotationInput);
+    }
+
+    private void FixedUpdate()
+    {
+        _userRot = _transform.rotation.eulerAngles;
+        _userRot += new Vector3(0, _rotationInput, 0);
+
+        _transform.rotation= Quaternion.Euler(_userRot);
+        _rigidbody.velocity = _transform.forward * _vertInput;
     }
 }
